4d19446f29a226e0890ce825b606db51c5ed612f
John Hayward <jhaywa02@gmail.com>
2022-12-12 17:52:51 -0600

    drawing pieces

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate piston;
 extern crate rand;
 
 use glutin_window::GlutinWindow as Window;
+use graphics::math::transform_pos;
 use graphics::rectangle::Border;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{Events, EventSettings};
@@ -16,12 +17,16 @@ type Color = [f32; 4];
 
 const WHITE: Color = [1.0; 4];
 const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
+const RED: Color = [1.0, 0.0, 0.0, 1.0];
+const GREEN: Color = [0.0, 1.0, 0.0, 1.0];
+const BLUE: Color = [0.0, 0.0, 1.0, 1.0];
+const YELLOW: Color = [1.0, 1.0, 0.0, 1.0];
 
 const START_SIZE: [f64; 2] = [320.0, 512.0];
 const PIXEL_SIZE: f64 = 32.0;
 const WORLD_SIZE: [f64; 2] = [START_SIZE[0]/PIXEL_SIZE, START_SIZE[1]/PIXEL_SIZE];
 
-
+#[derive(Clone)]
 struct Piece {
     shape: i32,
     rotation: f64,
@@ -46,10 +51,12 @@ impl Piece {
     }
 }
 
-type Map = Vec<Vec<i32>>;
+type Map = Vec<Vec<Piece>>;
 
 fn build_map() -> Map {
-    let map: Map = vec![vec![0; WORLD_SIZE[1] as usize]; WORLD_SIZE[0] as usize];
+    let mut map: Map = vec![vec![Piece::new(0); WORLD_SIZE[1] as usize]; WORLD_SIZE[0] as usize];
+
+    map[1][2] = Piece::new(4);
 
     map
 }
@@ -62,6 +69,9 @@ fn main() {
     let mut window: Window = settings.build().expect("Error Creating Window");
 
     let mut gl = GlGraphics::new(opengl);
+    let map = build_map();
+
+    println!("{}, {}", map.len(), map[0].len());
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
@@ -79,12 +89,21 @@ fn main() {
                             PIXEL_SIZE * (j + 1) as f64,
                         ];
 
-                        graphics::Rectangle::new_border(WHITE, PIXEL_SIZE).border().draw(
+                        graphics::Rectangle::new_border(BLACK, 2.0).draw(
                             pos,
                             &c.draw_state,
                             c.transform,
                             g,
                         );
+
+                        if map[i as usize][j as usize].shape != 0 {
+                            graphics::Rectangle::new_border(BLACK, 2.0).color(YELLOW).draw(
+                                pos,
+                                &c.draw_state,
+                                c.transform,
+                                g,
+                            );
+                        }
                     }
                 }
             })
