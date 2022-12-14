3b037dbb6883331fbd56220a36a044304c97c9d9
John Hayward <jhaywa02@gmail.com>
2022-12-14 13:56:58 -0600

    rotation finished

--- src/libs/pieces_src/pieces.rs
+++ src/libs/pieces_src/pieces.rs
@@ -1,22 +1,289 @@
 use crate::libs::constants::app_constants::*;
 
-pub type PieceShape = [[i32; 3]; 3];
+pub type Shape = [[i32; 2]; 4];
 
-pub enum PieceNames {
-    ShapeNone = 0,
-    ShapeI = 1,
-    ShapeJ = 2,
-    ShapeL = 3,
-    ShapeO = 4,
-    ShapeS = 5,
-    ShapeT = 6,
-    ShapeZ = 7,
+//Relate Shape type to number
+#[derive(Clone, Copy)]
+pub enum PieceType {
+    None,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z,
 }
 
+//Piece structure
+#[derive(Clone, Copy)]
 pub struct Piece {
-    pub piece: PieceNames,
-    pub shape: PieceShape,
+    pub piece: PieceType,
+    pub shape: Shape,
+    pub active: bool,
     pub rotation: i32,
     pub position: [i32; 2],
     pub color: Color,
+}
+
+fn shape_piece(piece: &PieceType, pos: [i32; 2], rotation: i32) -> Shape {
+    let base_shape = match piece {
+        PieceType::None => {[
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+        ]},
+        PieceType::I => {
+            match rotation % 2 {
+                0 => [
+                    [-2, 0],
+                    [-1, 0],
+                    [0, 0],
+                    [1, 0],
+                ],
+                1 => [
+                    [0, -1],
+                    [0, -1],
+                    [0, 0],
+                    [0, 1],
+                ],
+                _ => {
+                    println!("Rotation Error");
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                    ]
+                }
+            }
+        },
+        PieceType::J => {
+            match rotation {
+                0 => [
+                    [-1, 0],
+                    [0, 0],
+                    [1, 0],
+                    [1, 1],
+                ],
+                1 => [
+                    [0, -1],
+                    [0, 0],
+                    [0, 1],
+                    [-1, 1],
+                ],
+                2 => [
+                    [-1, -1],
+                    [-1, 0],
+                    [0, 0],
+                    [1, 0],
+                ],
+                3 => [
+                    [1, 0],
+                    [0, -1],
+                    [0, 0],
+                    [0, 1],
+                ],
+                _ => {
+                    println!("Rotation Error");
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                    ]
+                }
+            }
+        },
+        PieceType::L => {
+            match rotation {
+                0 => [
+                    [-1, 1],
+                    [-1, 0],
+                    [0, 0],
+                    [1, 0],
+                ],
+                1 => [
+                    [-1, -1],
+                    [0, -1],
+                    [0, 0],
+                    [0, 1],
+                ],
+                2 => [
+                    [1, -1],
+                    [1, 0],
+                    [0, 0],
+                    [-1, 0],
+                ],
+                3 => [
+                    [0, -1],
+                    [0, 0],
+                    [0, 1],
+                    [1, 1],
+                ],
+                _ => {
+                    println!("Rotation Error");
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                    ]
+                }
+            }
+        },
+        PieceType::O => {[
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [1, 1],
+        ]}
+        PieceType::S => {
+            match rotation % 2 {
+                0 => [
+                    [-1, 1],
+                    [0, 1],
+                    [0, 0],
+                    [1, 0],
+                ],
+                1 => [
+                    [-1, -1],
+                    [-1, 0],
+                    [0, 0],
+                    [0, 1],
+                ],
+                _ => {
+                    println!("Rotation Error");
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                    ]
+                }
+            }
+        },
+        PieceType::T => {
+            match rotation {
+                0 => [
+                    [-1, 0],
+                    [0, 0],
+                    [0, 1],
+                    [1, 0],
+                ],
+                1 => [
+                    [0, -1],
+                    [-1, 0],
+                    [0, 0],
+                    [0, 1],
+                ],
+                2 => [
+                    [-1, 0],
+                    [0, 0],
+                    [0, -1],
+                    [1, 0],
+                ],
+                3 => [
+                    [0, -1],
+                    [1, 0],
+                    [0, 0],
+                    [0, 1],
+                ],
+                _ => {
+                    println!("Rotation Error");
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                    ]
+                }
+            }
+        },
+        PieceType::Z => {
+            match rotation % 2 {
+                0 => [
+                    [-1, 0],
+                    [0, 0],
+                    [0, 1],
+                    [1, 1],
+                ],
+                1 => [
+                    [0, -1],
+                    [0, 0],
+                    [-1, 0],
+                    [-1, 1],
+                ],
+                _ => {
+                    println!("Rotation Error");
+                    [
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                    ]
+                }
+            }
+        },
+    };
+
+    let mut shape: Shape = [[0; 2]; 4];
+    let mut counter = 0;
+    for cord in base_shape {
+        let new_cords = [(cord[0] + pos[0]), (cord[1] + pos[1])];
+        shape[counter] = new_cords;
+        counter += 1;
+    }
+
+    shape
+}
+
+impl Piece {
+    pub fn new(piece: PieceType) -> Self {
+        Piece {
+            piece,
+            shape: shape_piece(&piece, [4, 0], 0),
+            rotation: 0,
+            position: [4, 0], //Center of piece defined at 4, 0
+            active: true,
+            color: match piece {
+                PieceType::None => WHITE,
+                PieceType::I => LIGHT_BLUE,
+                PieceType::J => BLUE,
+                PieceType::L => ORANGE,
+                PieceType::O => YELLOW,
+                PieceType::S => GREEN,
+                PieceType::T => PURPLE,
+                PieceType::Z => RED,
+            }
+        }
+    }
+
+    pub fn fall(&mut self) {
+        let pos = self.position;
+        let new_pos = [pos[0], pos[1]+1];
+
+
+        self.shape = shape_piece(&self.piece, new_pos, self.rotation);
+        self.position = new_pos;
+
+    }
+
+    pub fn rotate(&mut self, dir: &str) {
+        if dir == "Clockwise" {
+            if self.rotation < 3 {
+                self.rotation += 1;
+            } else {
+                self.rotation = 0;
+            }
+        } else if dir == "Counter" {
+            if self.rotation > 0 {
+                self.rotation -= 1;
+            } else {
+                self.rotation = 3;
+            }
+        }
+        self.shape = shape_piece(&self.piece, self.position, self.rotation);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,29 @@ use piston::event_loop::{Events, EventSettings};
 use piston::input::{ButtonState, Button, Key};
 use piston::{ButtonEvent, RenderEvent, WindowSettings};
 
+use rand::Rng;
 
 use tetris::libs::constants::app_constants::*;
 use tetris::libs::map_src::map::*;
+use tetris::libs::pieces_src::pieces::*;
 
+
+fn rand_piece() -> Piece {
+    let rand_num = rand::thread_rng().gen_range(1..=7);
+
+    let piece = match rand_num {
+        1 => PieceType::I,
+        2 => PieceType::J,
+        3 => PieceType::L,
+        4 => PieceType::O,
+        5 => PieceType::S,
+        6 =>PieceType::T,
+        7 => PieceType::Z,
+        _ => PieceType::None,
+    };
+
+    Piece::new(piece)
+}
 
 fn main() {
 
@@ -24,6 +43,7 @@ fn main() {
 
     let mut gl = GlGraphics::new(opengl);
     let mut _map = build_map();
+    let mut start_piece = rand_piece();
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
@@ -42,6 +62,10 @@ fn main() {
                             PIXEL_SIZE * (j + 1) as f64,
                         ];
 
+                        if start_piece.shape.contains(&[i, j]) {
+                            color = start_piece.color;
+                        }
+
                         graphics::Rectangle::new_border(BLACK, 2.0).color(color).draw(
                             pos,
                             &c.draw_state,
@@ -55,7 +79,15 @@ fn main() {
         if let Some(k) = e.button_args() {
             if k.state == ButtonState::Press {
                 match k.button {
-                    Button::Keyboard(Key::Space) => {},
+                    Button::Keyboard(Key::Space) => {
+                        start_piece.fall();
+                    },
+                    Button::Keyboard(Key::S) => {
+                        start_piece.rotate("Counter");
+                    },
+                    Button::Keyboard(Key::W) => {
+                        start_piece.rotate("Clockwise");
+                    }
                     _ => {},
                 }
             }
